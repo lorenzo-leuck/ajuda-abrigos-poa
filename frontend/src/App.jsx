23fe49015dc5d737e9d45620a15769e6c6479318
lorenzo-leuck <lorenzo@outlook.com>
2024-05-12 00:58:08 -0300

    material-ui wip

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavigationBar from './components/NavigationBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Welcome to the Shelter Donation Manager</h1>
+      <h1> Abrigo centro vida</h1>
+      <NavigationBar />
     </div>
   );
 }
