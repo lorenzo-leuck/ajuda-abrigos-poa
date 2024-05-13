bf9f4bc9c0801fbfc8279dffb7acb5bdba287831
lorenzo-leuck <lorenzo@outlook.com>
2024-05-13 12:44:57 -0300

    meui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import NavigationBar from './components/NavigationBar'
+// import NavigationBar from './components/NavigationBar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1> Abrigo centro vida</h1>
-      <NavigationBar />
+      {/* <NavigationBar /> */}
     </div>
   );
 }
