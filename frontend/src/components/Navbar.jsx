eeab7c911e4bc189cc991a389dd12673a7b8f7ce
lorenzo-leuck <lorenzo@outlook.com>
2024-05-14 00:43:54 -0300

    auth ok

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,30 +1,23 @@
 import React, { useState } from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
+import {
+  AppBar, Toolbar, Typography, Box, Container, IconButton, Menu, MenuItem,
+  Tooltip, FormGroup, FormControlLabel, Switch, Avatar
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import { Link } from 'react-router-dom';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import Switch from '@mui/material/Switch';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import LoginIcon from '@mui/icons-material/Login';
+import { Link } from 'react-router-dom';
 
 const pages = ['Doações', 'Voluntários'];
 const pageRoutes = ['/doacoes', '/voluntarios'];
 
 const settings = ['Editar', 'Contatos', 'Sair'];
-const settingsRoutes = ['/editar', '/contatos', '/logout'];
+const settingsRoutes = ['/editar', '/contatos', '/login'];
 
 const Navbar = () => {
   const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
-  const [auth, setAuth] = React.useState(true);
+  const [auth, setAuth] = useState(true);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -43,40 +36,25 @@ const Navbar = () => {
   };
 
   const handleChange = (event) => {
-    console.log(event.target.checked);
     setAuth(event.target.checked);
   };
 
-
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setAuth(false);
+    window.location.href = '/doacoes';
+  };
 
   return (
     <AppBar color="secondary">
       <Container>
         <Toolbar disableGutters>
-          <Box>
-            
-            
-          {/* <FormGroup>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={auth}
-              onChange={handleChange}
-              aria-label="login switch"
-            />
-          }
-          label={auth ? 'Logout' : 'Login'}
-        />
-    </FormGroup>
- */}
-
-
-
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
 
 
             <IconButton
               size="large"
-              aria-label="account of current user"
+              aria-label="menu"
               aria-controls="menu-appbar"
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
@@ -85,22 +63,14 @@ const Navbar = () => {
               <MenuIcon />
             </IconButton>
             <Menu
-              id="menu-appbar"
+              id="menu-nav"
               anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
               keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
+              transformOrigin={{ vertical: 'top', horizontal: 'left' }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
+              sx={{ display: { xs: 'block', md: 'none' } }}
             >
               {pages.map((page, index) => (
                 <MenuItem key={page} component={Link} to={pageRoutes[index]} onClick={handleCloseNavMenu}>
@@ -110,50 +80,38 @@ const Navbar = () => {
             </Menu>
           </Box>
 
-          <Typography variant="h6" sx={{ flexGrow: 20, textAlign: 'center', fontFamily: 'Roboto'}}>
+          <Typography variant="h6" sx={{ flexGrow: 1, textAlign: 'center', fontFamily: 'Roboto' }}>
             Abrigo centro vida
           </Typography>
 
-
           {auth ? (
-  <Box sx={{ flexGrow: 0 }}>
-    <Tooltip>
-      <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }} color="inherit" size="large">
-        <AccountCircle />
-      </IconButton>
-    </Tooltip>
-    <Menu
-      sx={{ mt: '45px' }}
-      id="menu-appbar"
-      anchorEl={anchorElUser}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={Boolean(anchorElUser)}
-      onClose={handleCloseUserMenu}
-    >
-      {settings.map((setting, index) => (
-        <MenuItem key={setting} component={Link} to={settingsRoutes[index]} onClick={handleCloseUserMenu}>
-          <Typography textAlign="center">{setting}</Typography>
-        </MenuItem>
-      ))}
-    </Menu>
-  </Box>
-) : (
-  <IconButton onClick={()=>{window.location.href = '/login';;}} sx={{ p: 0 }} color="inherit" size="large">
-    <LoginIcon />
-  </IconButton>
-)}
-
-
-
-
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Account settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }} color="inherit">
+                  <AccountCircle />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                id="menu-user"
+                anchorEl={anchorElUser}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                keepMounted
+                transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting, index) => (
+                  <MenuItem key={setting} component={Link} to={settingsRoutes[index]} onClick={setting === 'Sair' ? handleLogout : handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          ) : (
+            <IconButton onClick={() => { window.location.href = '/login'; }} sx={{ p: 0 }} color="inherit">
+              <LoginIcon />
+            </IconButton>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
