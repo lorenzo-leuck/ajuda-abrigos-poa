72e548a7384d1cd05e95eb830f238ad29dcd91cc
lorenzo-leuck <lorenzo@outlook.com>
2024-05-16 10:39:12 -0300

    wip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    target: 'esnext',
+    platform: 'linux',
+  },
   plugins: [react()],
 })
