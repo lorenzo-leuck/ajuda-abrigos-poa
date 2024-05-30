5e50a130fd74803e208db709b5fe578241256744
lorenzo-leuck <lorenzo@outlook.com>
2024-05-29 23:31:39 -0300

    8080

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 
 let baseUrl;
 if (projectEnvironment === 'production') {
-  baseUrl = 'https://ajudaabrigospoa.com.br:1339';
+  baseUrl = 'https://ajudaabrigospoa.com.br:8080';
 } else {
   baseUrl = 'http://localhost:1339';
 }
